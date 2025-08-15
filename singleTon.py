dc0d62544cb22faf85a47ac9c9f9ebb0cfd16059
Abhishek Rout <abhishek1rout@gmail.com>
2025-08-16 00:23:09 +0530

    pushing some design patterns

--- singleTon.py
+++ singleTon.py
@@ -14,4 +14,19 @@ print(o2,o2.data)
 o2.data=5
 print(o1.data,o2.data)
 
-# In the below method it refered to different memeory location but shared the state
+# In the below method it refered to different memeory location but shared the state also known as Mono State Pattern i.e. different object same property
+class temp(object):
+    _shared = {}
+    def __init__(self):
+        self.__dict__ = self._shared
+class single_2(temp):
+    def __init__(self, arg):
+        temp.__init__(self)
+        self.val = arg
+o1 = single_2("HII")
+print(o1, o1.val, o1.__dict__)
+o2 = single_2("Hello")
+print(o2,o2.val,o2.__dict__)
+print(o1, o1.val, o1.__dict__)
+
+# The below method is decorator method
